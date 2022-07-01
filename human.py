7c3cf70aa0b08ac6dcc0023e07729f7e5423a2bd
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-07-01 10:28:09 +0530

    Update human.py

--- human.py
+++ human.py
@@ -12,14 +12,14 @@ def add_delay(num_timestep):
         time_counter += 1
 
 i=0
-commands=["sit","stand"]
+commands=["sit","stand","walk","stand"]
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     
     while(i<20):
-        info = bytes(commands[i%2], 'utf-8')
+        info = bytes(commands[i%4], 'utf-8')
         emitter.send(info)
         add_delay(50)
         i=i+1
